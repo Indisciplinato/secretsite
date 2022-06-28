5ce4e30c56626c240d194d42e9ad6d0b2c69f62f
Indisciplinato <64929429+Indisciplinato@users.noreply.github.com>
2022-06-28 11:30:45 +0200

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -4,7 +4,7 @@ export default function Footer() {
   return (
     <>
       <footer className={styles.footer}>
-        Made with <img src="/netliheart.svg" alt="Netlify Logo" className={styles.logo} /> for you
+        Fatto con <img src="/poop.png" alt="Poop" className={styles.logo} /> per nessuno.
       </footer>
     </>
   )
